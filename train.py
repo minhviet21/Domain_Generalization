2f92db694aec00c863cdacd87bd307aeca9e6984
minhviet21 <128829837+minhviet21@users.noreply.github.com>
2024-04-27 23:53:40 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,13 +1,20 @@
 import torch
 import torch.optim as optim
+from torch import nn
 from model.ResnetBase import ResnetModel
-from dataset.transform import train_loader
+from dataset.transform import transform
+from dataset.OfficeHome import OfficeHomeDataset
+
+data_dir = "/content/drive/MyDrive/OfficeHome"
+domains = ["Art", "Clipart"]
+
+dataset = OfficeHomeDataset(root_dir=data_dir, domains=domains, transform=transform)
+train_loader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
 
 model = ResnetModel(num_classes=65)
 model.set_optimizer(optim.Adam(model.parameters(), lr=0.001))
 model.set_loss_fn(nn.CrossEntropyLoss())
 model.set_scheduler(optim.lr_scheduler.StepLR(model.optimizer, step_size=5, gamma=0.1))
-
 for epoch in range(10):
     for x, y in train_loader:
         loss = model.update(x, y)
