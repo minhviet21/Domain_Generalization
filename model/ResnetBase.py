cf4082e9ce2c9d414b4a1c17b72f03f8b2a301ee
minhviet21 <vmdiioenathn@gmail.com>
2024-04-28 16:50:05 +0700

    update rsc

--- model/ResnetBase.py
+++ model/ResnetBase.py
@@ -1,17 +1,46 @@
 from torchvision.models import resnet50
 from torch import nn
 
-class ResnetModel(nn.Module):
-    def __init__(self, num_classes):
-        super(ResnetModel, self).__init__()
-        self.resnet = resnet50(weights=None)
-        self.resnet.fc = nn.Linear(2048, num_classes)
-    
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
     def forward(self, x):
+        return x
+
+class Featurizer(nn.Module):
+    def __init__(self, num_classes):
+        super(Featurizer, self).__init__()
+        self.resnet = resnet50(weights=None)
+        
+        del self.resnet.fc
+        self.resnet.fc = Identity()  
+
+    def forward(self,x):
         return self.resnet(x)
+
+def Classifier(in_features, out_features, is_nonlinear=False):
+    if is_nonlinear:
+        return nn.Sequential(
+            nn.Linear(in_features, in_features // 2),
+            nn.ReLU(),
+            nn.Linear(in_features // 2, in_features // 4),
+            nn.ReLU(),
+            nn.Linear(in_features // 4, out_features))
+    else:
+        return nn.Linear(in_features, out_features)
+
+class ResnetBase(nn.Module):
+    def __init__(self, num_classes, is_nonlinear=False):
+        super(ResnetBase, self).__init__()
+        self.featurizer = Featurizer(num_classes)
+        self.classifier = Classifier(2048, num_classes, is_nonlinear)
+        self.network = nn.Sequential(self.featurizer, self.classifier)
+
+    def forward(self, x):
+        return self.network(x)
     
     def update(self, x, y):
-        outputs = self(x)
+        outputs = self.forward(x)
         loss = self.loss_fn(outputs, y)
         self.optimizer.zero_grad()
         loss.backward()
@@ -29,3 +58,6 @@ class ResnetModel(nn.Module):
     
     def update_lr(self):
         self.scheduler.step()
+
+a= ResnetBase(10, True)
+print(a)
